b89989493f983568bb3e8b14c459137168859cb5
Zachary Cotton <zacharycotton@berkeley.edu>
2019-03-21 23:16:17 -0700

    Backup Manager (#16)

--- docker/images/backup-manager/backup.sh
+++ docker/images/backup-manager/backup.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo "Hi"
+eval `ssh-agent`
+ssh-add /backup-key
+sleep 1
+rm -rf repo/*
+rm -rf repo/.git
+git clone git@bitbucket.org:kngnm/mediaserver.git repo
+cd repo
+
+while true; do
+  ls -a
+  git add *
+  git commit -m "`date`"
+  git push origin master
+  sleep 10
+done
