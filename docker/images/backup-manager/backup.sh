ffa494b8756b15d7931a7541aaf4bd68d004a53b
Zachary Cotton <zacharycotton@berkeley.edu>
2019-03-24 23:04:34 -0700

    Backup Manager Does Not Need Password to Push Changes (#16)

--- docker/images/backup-manager/backup.sh
+++ docker/images/backup-manager/backup.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 eval `ssh-agent`
-ssh-add /backup-key
+ssh-add backup-key
 sleep 1
 rm -rf repo/*
 rm -rf repo/.git
-git clone git@bitbucket.org:kngnm/mediaserver.git repo
+echo yes | git clone $REPO repo
 cd repo
 
 while true; do
-  ls -a
   git add *
   git commit -m "`date`"
   git push origin master
-  sleep 10
+  sleep 600
 done
